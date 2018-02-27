1e10ec3133bcaf550ee58d19aa2b0094f52d2dd3
inyoka <simonmarkholland@gmail.com>
2018-02-27 09:10:37 +0700

    Basic botcatcher

--- basicapp/forms.py
+++ basicapp/forms.py
@@ -1,6 +1,16 @@
 from django import forms
+from django.core import validators
+
+
+def chack_for_z(value):
+    if value[0].lower() != 'z':
+        raise forms.ValidationError("Name needs to start with Z")
+
 
 class FormName(forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[chack_for_z])
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
+    botcatcher = forms.CharField(required=False,
+                                 widget=forms.HiddenInput,
+                                 validators=[validators.MaxLengthValidator(0)])
